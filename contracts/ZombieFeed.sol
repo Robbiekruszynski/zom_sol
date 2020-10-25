cc783436d33c0210bcdc91d7b996abfd93bc632a
robbie <robbiekruszynski@gmail.com>
2020-10-24 19:32:06 -0700

    update ZombueFeed

--- contracts/ZombieFeed.sol
+++ contracts/ZombieFeed.sol
@@ -11,4 +11,11 @@ contract ZombieFeeding is ZombieFactory {
     {
         require(ownerZombieCount[msg.sender] == 0); //look into ths line
     }
+
+     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+
+//We're going to need to get this zombie's DNA. So the next thing our function should do is declare a local Zombie named myZombie (which will be a storage pointer). Set this variable to be equal to index _zombieId in our zombies array.
 }
+
